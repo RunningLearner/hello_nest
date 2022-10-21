05244bcae7fca4683179566c0ea4568f38010cf4
RunningLearner <tkacnsdms@naver.com>
2022-10-21 20:40:19 +0900

    feat: movie search route added

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,20 +15,33 @@ export class MoviesController {
   getAll() {
     return 'this will return all movies.';
   }
-  @Get('/:id')
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `we are searching for a movie after: ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') id: string) {
     return `this wil return one movie id: ${id}`;
   }
+
   @Post()
-  create() {
-    return `this will creat movie`;
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
   }
-  @Delete('/:id')
-  remove(@Param(':id') id: string) {
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
     return `this will remove a movie id:${id}`;
   }
-  @Patch('/:id')
-  update(@Param('/:id') id: string) {
-    return `this will update a movie id:${id}`;
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateData) {
+    return {
+      updateMovieId: id,
+      ...updateData,
+    };
   }
 }
